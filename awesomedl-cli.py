0bc36159a99b9ec4d3d2833e29777ba9021f520f
Jinal Kothari <jinalspkothari@gmail.com>
2015-11-12 00:12:08 +0530

    Reformatting code, white spaces and such. No code change

--- awesomedl-cli.py
+++ awesomedl-cli.py
@@ -3,28 +3,29 @@ import six
 import re
 import webbrowser
 import xmltodict
-
 from lxml import html
 
-def extract_download_link(link):
-	start = link.find('url/') + 4
-	return link[start:]
 
-show_name=six.moves.input("Please enter Show name: ")
-url = "http://awesomedl.ru/?" + urllib.parse.urlencode({'feed':'rss2','s':show_name})
+def extract_download_link(link):
+    start = link.find('url/') + 4
+    return link[start:]
+
+
+show_name = six.moves.input("Please enter Show name: ")
+url = "http://awesomedl.ru/?" + urllib.parse.urlencode({'feed': 'rss2', 's': show_name})
 searchpage = urllib.request.urlopen(url).read()
 result = xmltodict.parse(searchpage)
 finalstr = []
 if 'rss' in result and 'channel' in result['rss'] and 'item' in result['rss']['channel']:
-	for episode in result['rss']['channel']['item']:
-		epilinks = html.fromstring(episode['content:encoded'])
-		links = []
-		for downlink in epilinks.xpath("//a"):
-			if str(downlink.text).lower() == 'mega':
-				links.append(extract_download_link(downlink.get("href")));
-		finalstr.append({'title':episode['title'], 'link':links})
-	[webbrowser.open(url,0,True) for url in finalstr[0]['link']]
-	print(finalstr)
+    for episode in result['rss']['channel']['item']:
+        epilinks = html.fromstring(episode['content:encoded'])
+        links = []
+        for downlink in epilinks.xpath("//a"):
+            if str(downlink.text).lower() == 'mega':
+                links.append(extract_download_link(downlink.get("href")));
+        finalstr.append({'title': episode['title'], 'link': links})
+    [webbrowser.open(url, 0, True) for url in finalstr[0]['link']]
+    print(finalstr)
 else:
-	print('no result found')
+    print('no result found')
 exit()
